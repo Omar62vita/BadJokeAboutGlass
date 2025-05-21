0279821746992c5c86d457f53333b1421c314747
Omar62vita <158755581+Omar62vita@users.noreply.github.com>
2025-05-21 22:21:11 +0400

    Update Glass.asm
    Full stupid rewrite

--- RoyAibel/Glass.asm
+++ RoyAibel/Glass.asm
@@ -1,65 +1,75 @@
 section .data
-    prefix      db "User drank 20%; "
-    nums        db 8, 6, 4, 2
-    suffix_tmpl db " 00% Left"
-    newline     db 10
-    message     db "Intern summoned; glass refilled"
-    quitMessage db "Intern quit; glass empty."
+    prefix        db "User just chugged ", 0
+    nums          db 8, 6, 4, 2            ; % left in glass each round
+    suffix_tmpl   db " 00% remains. ", 0
+    newline       db 10
+    message       db "Intern appears; glass magically full again.", 0
+    quitMessage   db "Intern gave up. User dehydrated.", 0
 
 section .bss
-    counter     resb 1
+    counter       resb 1                   ; how many rounds of thirst we tolerate
 
 section .text
     global _start
 
 _start:
+    ; Begin with full patience (4 rounds)
     mov byte [counter], 4
 
 round_loop:
-    xor esi, esi
+    xor esi, esi                           ; reset drink index
 
 print_loop:
+    ; Write prefix: "User just chugged "
     mov eax, 4
     mov ebx, 1
     mov ecx, prefix
-    mov edx, 16
+    mov edx, 20
     int 0x80
 
+    ; Get percentage value (nums[esi]) * 10
     mov al, [nums + esi]
     xor ah, ah
-    imul ax, ax, 10
-    mov dl, 10
-    div dl
-    add al, '0'
-    mov [suffix_tmpl + 1], al
-    add ah, '0'
-    mov [suffix_tmpl + 2], ah
+    imul ax, ax, 10                        ; multiply by 10 to get percentage
 
+    mov dl, 10
+    div dl                                 ; AX / 10 → AL = tens, AH = ones
+
+    ; Insert digits into suffix string
+    add al, '0'
+    mov [suffix_tmpl + 1], al              ; tens place
+    add ah, '0'
+    mov [suffix_tmpl + 2], ah              ; ones place
+
+    ; Write suffix: " 00% remains."
     mov eax, 4
     mov ebx, 1
     mov ecx, suffix_tmpl
-    mov edx, 10
+    mov edx, 16
     int 0x80
 
     inc esi
     cmp esi, 4
     jl print_loop
 
+    ; Determine which dramatic message to display
     mov eax, 4
     mov ebx, 1
     cmp byte [counter], 1
     jne .not_last
+
     mov ecx, quitMessage
-    mov edx, 26
+    mov edx, 32
     jmp .print_message
 
 .not_last:
     mov ecx, message
-    mov edx, 31
+    mov edx, 40
 
 .print_message:
     int 0x80
 
+    ; Add a newline after message, unless it's the final message
     cmp byte [counter], 1
     je .skip_newline
 
@@ -70,10 +80,13 @@ print_loop:
     int 0x80
 
 .skip_newline:
+    ; Decrease patience level
     dec byte [counter]
     cmp byte [counter], 0
     jg round_loop
 
+    ; Exit peacefully
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
